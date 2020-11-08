ab320dbc30097d66818b5ab964c7b9f03328bdec
Ryo Ioi <r.ioi@sms.ed.ac.uk>
2020-11-08 16:04:45 +0900

    update image1.py to move joints

---
+++ ryo/image1.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+import roslib
+import sys
+import rospy
+import cv2
+import numpy as np
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from std_msgs.msg import Float64MultiArray, Float64
+from cv_bridge import CvBridge, CvBridgeError
+
+
+class image_converter:
+
+  # Defines publisher and subscriber
+  def __init__(self):
+    # initialize the node named image_processing
+    rospy.init_node('image_processing', anonymous=True)
+    # initialize a publisher to send images from camera1 to a topic named image_topic1
+    self.image_pub1 = rospy.Publisher("image_topic1",Image, queue_size = 1)
+    # initialize a subscriber to recieve messages rom a topic named /robot/camera1/image_raw and use callback function to recieve data
+    self.image_sub1 = rospy.Subscriber("/camera1/robot/image_raw",Image,self.callback1)
+    # initialize the bridge between openCV and ROS
+    self.bridge = CvBridge()
+
+    ########
+    self.robot_joint1_pub = rospy.Publisher("/robot/joint1_position_controller/command", Float64, queue_size=10)
+    self.robot_joint2_pub = rospy.Publisher("/robot/joint2_position_controller/command", Float64, queue_size=10)
+    self.robot_joint3_pub = rospy.Publisher("/robot/joint3_position_controller/command", Float64, queue_size=10)
+    self.t0 = rospy.get_time()
+    self.cur_time = 0
+    ########
+
+  # Recieve data from camera 1, process it, and publish
+  def callback1(self,data):
+    ########
+    self.cur_time = np.array([rospy.get_time()]) - self.t0
+    ########
+    # Recieve the image
+    try:
+      self.cv_image1 = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+    
+    # Uncomment if you want to save the image
+    #cv2.imwrite('image_copy.png', cv_image)
+
+    im1=cv2.imshow('window1', self.cv_image1)
+    cv2.waitKey(1)
+
+    ########
+    x_d = (np.pi / 2) * np.sin(self.cur_time * np.pi / 15)
+    y_d = (np.pi / 2) * np.sin(self.cur_time * np.pi / 18)
+    z_d = (np.pi / 2) * np.sin(self.cur_time * np.pi / 20)
+    #self.joints.data = np.array([x_d, y_d, z_d])
+
+    self.joint1=Float64()
+    self.joint1.data= np.array(x_d)
+    self.joint2=Float64()
+    self.joint2.data= np.array(y_d)
+    self.joint3=Float64()
+    self.joint3.data= np.array(z_d)
+    ########
+
+    # Publish the results
+    try: 
+      self.image_pub1.publish(self.bridge.cv2_to_imgmsg(self.cv_image1, "bgr8"))
+      ########
+      self.robot_joint1_pub.publish(self.joint1)
+      self.robot_joint2_pub.publish(self.joint2)
+      self.robot_joint3_pub.publish(self.joint3)
+      ########
+    except CvBridgeError as e:
+      print(e)
+
+# call the class
+def main(args):
+  ic = image_converter()
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv2.destroyAllWindows()
+
+# run the code if the node is called
+if __name__ == '__main__':
+    main(sys.argv)
+
+
